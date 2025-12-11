4e748678f7359f5d7a32ea22ec770c05de6daedc
Keepwalking1989 <john.mcclainpramotion@gmail.com>
2025-12-11 19:54:18 +0530

    Fix: Force SCRIPT_NAME to /index.php to fix API routing on Vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,7 @@
 <?php
 
+
+// Fix for Vercel: Force script name to root index.php to prevent Laravel from stripping '/api' prefix
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+
 require __DIR__ . '/../public/index.php';
